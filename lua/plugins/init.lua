89ebc9235c6deb240108ae2928346a5d3eee1a24
Marco Yuen <marcoy@gmail.com>
2024-03-03 20:36:09 -0500

    flash configuration

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -54,21 +54,11 @@ return {
       }
     },
     keys = {
-      {
-        "s",
-        mode = { "n", "x", "o" },
-        function()
-          -- default options: exact mode, multi window, all directions, with a backdrop
-          require("flash").jump()
-        end,
-      },
-      {
-        "S",
-        mode = { "o", "x" },
-        function()
-          require("flash").treesitter()
-        end,
-      },
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
   },
   { "windwp/nvim-autopairs", config = true, },
