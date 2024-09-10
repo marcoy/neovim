a44ccd25578e34a6a9b578d67581d2a358f1bac9
Marco Yuen <marcoy@gmail.com>
2024-09-10 19:27:58 -0400

    Add obsidian.nvim

--- lua/plugins/notes.lua
+++ lua/plugins/notes.lua
@@ -66,4 +66,21 @@ return {
   --   end,
   --   ft = "markdown",
   -- },
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*",  -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "Knowledge",
+          path = "~/Documents/Knowledge",
+        },
+      },
+    }
+  },
 }
